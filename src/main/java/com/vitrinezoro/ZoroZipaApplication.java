e4b5cd7d4b72155aad54664202771ea4d56dfd5d
Junior Minko <son.minko2024@esatic.edu.ci>
2026-08-03 18:29:41 +0000

    Initial commit Zoro Zipa

---
+++ src/main/java/com/vitrinezoro/ZoroZipaApplication.java
@@ -0,0 +1,13 @@
+package com.vitrinezoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoroZipaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoroZipaApplication.class, args);
+	}
+
+}
